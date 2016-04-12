3207223facba6cc9a3238fb045b2c9b62c974438
Mike Fossel <mikefossel@gmail.com>
2016-04-11 20:28:24 -0700

    nuget packages added

---
+++ HotelManager.Core/WebApplication1/Infrastructure/BaseAPIController.cs
@@ -0,0 +1,27 @@
+﻿using HotelManager.Core.Domain;
+using HotelManager.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Infrastructure
+{
+    public class BaseApiController : ApiController
+    {
+        protected readonly IUserRepository _UserRepository;
+
+        public BaseApiController(IUserRepository userRepository)
+        {
+            _UserRepository = userRepository;
+        }
+
+        protected User CurrentUser
+        {
+            get
+            {
+                return _UserRepository.GetFirstOrDefault(u => u.UserName == User.Identity.Name);
+            }
+        }
+    }
+}
