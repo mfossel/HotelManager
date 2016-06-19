4be623189a5401a594bc07116835d9aaea278955
Mike Fossel <mikefossel@gmail.com>
2016-06-19 15:17:51 -0700

    updated room controller

---
+++ HotelManager.Core/HotelManager.Web/js/room/room.grid.js
@@ -0,0 +1,20 @@
+﻿angular.module('app').controller('RoomGridController', function ($scope, RoomResource) {
+
+    function activate() {
+        $scope.rooms = RoomResource.query();
+    }
+
+    activate();
+
+    $scope.deleteRoom = function (room) {
+        room.$remove(function (data) {
+            toastr.error('Room Deleted');
+            activate();
+        })
+
+    };
+
+
+
+
+});
