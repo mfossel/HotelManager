2df5f6ca73daf235c19273feb403b3eff8cb80ac
Mike Fossel <mikefossel@gmail.com>
2016-06-19 15:15:24 -0700

    added room resource

---
+++ HotelManager.Core/HotelManager.Web/js/room/room.resource.js
@@ -0,0 +1,9 @@
+﻿angular.module('app').factory('RoomResource', function (apiUrl, $resource) {
+    return $resource(apiUrl + '/rooms/:roomId', { roomId: '@RoomId' },
+        {
+            'update': {
+                method: 'PUT'
+            }
+
+        });
+});
